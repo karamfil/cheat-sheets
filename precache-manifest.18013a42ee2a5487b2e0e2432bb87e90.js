e8f5c66f9012f3798cc12f1a5d0b8078c0f07a8a
Alexander Ivanov <aivanov@shutterfly.com>
2019-02-20 00:11:56 +0200

    Updates

---
+++ precache-manifest.18013a42ee2a5487b2e0e2432bb87e90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6a520241d3c976f84e4",
+    "url": "/cheat-sheets/static/css/main.983b2803.chunk.css"
+  },
+  {
+    "revision": "c6a520241d3c976f84e4",
+    "url": "/cheat-sheets/static/js/main.c6a52024.chunk.js"
+  },
+  {
+    "revision": "87269fef07562c2fe9d2",
+    "url": "/cheat-sheets/static/js/runtime~main.87269fef.js"
+  },
+  {
+    "revision": "b2f5ab1217bac76f1d55",
+    "url": "/cheat-sheets/static/js/2.b2f5ab12.chunk.js"
+  },
+  {
+    "revision": "6801771a56b4db61620650190d82e5d7",
+    "url": "/cheat-sheets/index.html"
+  }
+];
